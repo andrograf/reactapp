6c07521c0e5656d1d206b0b5af26dac59813b14d
Patricia P <andrograf31@gmail.com>
2023-03-18 19:21:58 +0100

    fix: remove empty lines

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,48 +4,47 @@ import styled from 'styled-components';
 const Section = styled.div`
  display: flex;
  justify-content: center;
- 
 `
+
 const Container = styled.div`
 padding: 10px 0px;
  width:1400px;
  display: flex;
  justify-content: space-between;
- 
 `
+
 const Links = styled.div`
  display: flex;
  align-items: center;
  gap: 50px
- 
 `
+
 const Icons = styled.div`
   display: flex;
  align-items: center;
  gap: 20px
-
- 
 `
+
 const Icon = styled.img`
  width: 20px;
  cursor: pointer;
 `
+
 const List = styled.ul`
  display: flex;
  justify-content: space-between;
  gap: 20px;
  list-style: none;
- 
 `
+
 const ListItem = styled.li`
  cursor: pointer;
- 
- 
 `
+
 const Logo = styled.img`
  height: 50px;
- 
 `
+
 const Button = styled.button`
  width: 100px;
  padding: 10px;
@@ -54,7 +53,6 @@ const Button = styled.button`
  border: none;
  border-radius: 5px;
  cursor: pointer;
- 
 `
 
 
