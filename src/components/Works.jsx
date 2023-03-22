ea8a8769699b7ca9f2d85440d329c8291016bc9e
Patricia P <andrograf31@gmail.com>
2023-03-22 11:59:13 +0100

    feat: add text, text animation to works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const datas = [
+    "Plant Trees",
+    "Recycling",
+    "Clean Ocieans",
+    "Save Wild Lifes",
+    "Charitate"
+];
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
@@ -10,30 +18,72 @@ const Section = styled.div`
 `
 
 const Container = styled.div`
-  height: 1400px;
-  scroll-snap-align: center;
+  width: 1400px;
   display: flex;
   justify-content: space-between;
 `
 
 const Left = styled.div`
 flex: 1;
+display: flex;
+align-items: center;
 `
 const List = styled.ul`
-
+list-style : none;
+display: flex;
+flex-direction: column;
+gap: 20px;
 `
 const ListItem = styled.li`
+font-size: 100px;
+font-weigth: bold;
+cursor: pointer;
+color: transparent;
+-webkit-text-stroke: 1px white;
+position: relative;
 
+
+    ::after{
+        content: "${(props)=>props.text}";
+        position: absolute;
+        top: 0;
+        left: 0;
+        color: white;
+        width: 0px;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+
+&:hover{
+    ::after{
+        animation: moveText 0.5s linear both;
+
+        @keyframes moveText {
+            to{
+                width: 100%;
+            }
+        }
+    }
+
+}
 `
 
 const Right = styled.div`
 flex: 1;
 `
+
 export default class Works extends Component {
     render() {
         return (
             <Section>
-            <div>Works</div>
+                <Container>
+                    <Left>
+                        <List>
+                            {datas.map((data) => <ListItem key={data} text={data} >{data}</ListItem>)}
+                        </List>
+                    </Left>
+                    <Right></Right>
+                </Container>
             </Section>
         )
     }
