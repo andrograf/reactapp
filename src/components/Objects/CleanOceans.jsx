7d2863f471c6f474828f4c44533c10bdffcc22d8
Patricia P <andrograf31@gmail.com>
2023-04-02 21:41:03 +0200

    feat: select lottie for 'CleanOcean' component

--- src/components/Objects/CleanOceans.jsx
+++ src/components/Objects/CleanOceans.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import styled from 'styled-components';
 
 const Section = styled.div`
-position: relative;
+display: flex;
+align-items: center;
+flex-direction: column;
 `
 const Top = styled.div`
 display: flex;
@@ -16,7 +18,6 @@ display: flex;
 justify-content: center;
 align-items: center;
 position: relative;
-
 `
 const Description = styled.p`
 width: 450px;
@@ -26,15 +27,39 @@ color: lightgray;
 `
 
 export default class CleanOceans extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lotti: {
+                width: "85%",
+                
+
+            }
+        }
+    }
     render() {
         return (
+            <Section>
+                <Top>
+                
             <lottie-player
                 autoplay
                 loop
                 mode="normal"
-                src="https://assets7.lottiefiles.com/private_files/lf30_qi37qiub.json"
-                style={{ width: "100%" }}>
-            </lottie-player>
+                        src="https://assets4.lottiefiles.com/packages/lf20_OFvr8Nvcmq.json"
+                        style={this.state.lotti}>
+                    </lottie-player>
+                </Top>
+                <Bottom>
+                    <Description>
+                        Every year, millions of tons of plastic enter the oceans, of which the majority spills out from rivers.
+                        A portion of this plastic travels to ocean garbage patches,
+                        where it gets caught in a vortex of circulating currents alongside plastic from other sources
+                        (e.g. offshore fishing activities). If no action is taken,
+                        plastic will increasingly impact our ecosystems, health, and economies.
+                    </Description>
+                </Bottom>
+            </Section>
         );
     }
 }
