5c6f8852ceb0e45e5c9982b69e0fa802851953c7
andrograf <patriciapusztai@gmail.com>
2023-08-24 20:25:29 +0200

    feat: add alternate text to image

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -31,7 +31,7 @@ export default class Hero extends Component {
                     <Right>
                         <Title>Ailing Planet</Title>
                         <WhatWeDo>
-                            <Line src="./img/line.png"></Line>
+                            <Line src="./img/line.png" alt="hyphen"></Line>
                             <Subtitle>You can make difference</Subtitle>
                         </WhatWeDo>
 
